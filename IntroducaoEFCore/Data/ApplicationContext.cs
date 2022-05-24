b11244debb55381bd0e702ad86b16ad3a921dd37
Jean Michael <jeanmmclaudino@gmail.com>
2022-05-23 22:11:49 -0300

    inserir dados em massa

--- IntroducaoEFCore/Data/ApplicationContext.cs
+++ IntroducaoEFCore/Data/ApplicationContext.cs
@@ -1,5 +1,7 @@
-﻿using IntroducaoEFCore.Domain;
+﻿using IntroducaoEFCore.Data.Configurations;
+using IntroducaoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,63 +12,27 @@ namespace IntroducaoEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
-         
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
 
         public DbSet<Pedido> Pedidos { get; set; } 
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> clientes { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server = JEANMICHAEL; Database = Teste; Trusted_Connection = True");
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Server = JEANMICHAEL; Database = Teste; Trusted_Connection = True");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Cliente>(p =>
-            {
-                p.ToTable("Clientes");
-                p.HasKey(p => p.Id);
-                p.Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
-                p.Property(p => p.Telefone).HasColumnType("CHAR(11)");
-                p.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
-                p.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
-                p.Property(p => p.Cidade).HasMaxLength(60).IsRequired();
+            modelBuilder.ApplyConfiguration(new ClienteConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoItemConfiguration());
+            modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
 
-                p.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone");
-
-            });
-
-            modelBuilder.Entity<Produto>(p =>
-                {
-                    p.ToTable("Produtos");
-                    p.HasKey(p => p.Id);
-                    p.Property(p => p.CodigoBarras).HasColumnType("VARCHAR(14)").IsRequired();
-                    p.Property(p => p.Descricao).HasColumnType("VARCHAR(60)");
-                    p.Property(p => p.Valor).IsRequired();
-                    p.Property(p => p.TipoProduto).HasConversion<string>();
-                });
-
-                modelBuilder.Entity<Pedido>(p =>
-                {
-                    p.ToTable("Pedido");
-                    p.HasKey(p => p.Id);
-                    p.Property(p => p.IniciadoEm).HasDefaultValueSql("GETDATE()").ValueGeneratedOnAdd();
-                    p.Property(p => p.Status).HasConversion<string>();
-                    p.Property(p => p.TipoFrete).HasConversion<string>();
-                    p.Property(p => p.Observacao).HasColumnType("VARCHAR(512)");
-
-                    p.HasMany(p => p.Itens)
-                    .WithOne(p => p.Pedido)
-                    .OnDelete(DeleteBehavior.Cascade);
-
-                });
-
-                modelBuilder.Entity<PedidoItem>(p =>
-                {
-                    p.ToTable("PedidoItens");
-                    p.HasKey(p => p.Id);
-                    Microsoft.EntityFrameworkCore.Metadata.Builders.PropertyBuilder<int> propertyBuilder = p.Property(p => p.Quantidade).IsRequired();
-                    p.Property(p => p.Valor).IsRequired();
-                    p.Property(p => p.Desconto).IsRequired();
-                });   
         }
     }
 }
--- IntroducaoEFCore/Program.cs
+++ IntroducaoEFCore/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using IntroducaoEFCore.Domain;
+using IntroducaoEFCore.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 
 namespace IntroducaoEFCore
@@ -16,9 +18,58 @@ namespace IntroducaoEFCore
                 // 
             }
 
-            Console.WriteLine("Hello, World.");
+            //InserirDados();
+            InserirDadosEmMassa();
 
+        }
 
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234564565454",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "Jean Michael",
+                CEP = "99999000",
+                Cidade = "Curitiba",
+                Estado = "PR",
+                Telefone = "41992119200"
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.AddRange(produto, cliente);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total Registro(s): {registros}");
+
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234564565454",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+            //db.Produtos.Add(produto);
+            //db.Set<Produto>().Add(produto);
+            //db.Entry(produto).State = EntityState.Added;
+            db.Add(produto);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total registro(s): {registros}");
         }
     }
 }
